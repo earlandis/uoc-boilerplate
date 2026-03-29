d9c3bea87e9de1aba23dc6010eb4e842a73558a9
Elisa Arlandis Daviu <earlandis@uoc.edu>
2026-03-29 12:58:54 +0200

    Header

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -5,12 +5,31 @@
 
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
+import Swiper from 'swiper';
+import 'swiper/swiper-bundle.css';
+import { Pagination, Autoplay } from 'swiper/modules';
 
 /**
  * Write any other JavaScript below
  */
-
-+(function () {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-})();
+document.addEventListener('DOMContentLoaded', () => {
+  new Swiper('.header__hero', {
+    effect : 'coverflow',
+    grabCursor : true,
+    centeredSlides: true,
+    slidesPerView : 'auto',
+    coverflowEffect: 
+    {
+      rotate: 0, 
+      stretch : 0, 
+      depth: 200, 
+      modifier: 1, 
+      slideShadows: true
+    },
+    modules: [Pagination, Autoplay],
+    loop: true,
+    autoplay: { delay: 4000 },
+    pagination: { el: '.swiper-pagination', clickable: true },
+    parallax: true
+  });
+});
